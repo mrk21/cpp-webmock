9786b54cab56977ecbc3bdb4232e8c37e2ce3f39
Yuichi Murata <mrk21info+github@gmail.com>
2014-12-17 16:21:21 +0900

    Added the directive/response.

---
+++ test/spec/directive/response_spec.cpp
@@ -0,0 +1,67 @@
+#include <bandit_with_gmock/bandit_with_gmock.hpp>
+#include <webmock/directive/response.hpp>
+#include <exception>
+
+namespace webmock { namespace directive {
+go_bandit([]{
+    using namespace bandit;
+    
+    describe("webmock::directive::response", []{
+        describe("response(response)", [&]{
+            it("should be the response", [&]{
+                response_sequence seq = response({"200"});
+                AssertThat(seq.get_response({}), Equals(webmock::response{"200"}));
+            });
+        });
+        
+        describe("response(generator)", [&]{
+            it("should be a response created by the generator", [&]{
+                response_sequence seq = response([](auto && req){
+                    return webmock::response{"200", req.method};
+                });
+                AssertThat(seq.get_response({"GET"}), Equals(webmock::response{"200","GET"}));
+                AssertThat(seq.get_response({"PUT"}), Equals(webmock::response{"200","PUT"}));
+            });
+        });
+        
+        describe("error<Exeption>(Types... args)", [&]{
+            it("should throw the Exception with the args", [&]{
+                struct my_exception: public std::exception {
+                    int a;
+                    std::string b;
+                    my_exception(std::string const & b) : a(1), b(b) {}
+                    my_exception(int a, std::string const & b) : a(a), b(b) {}
+                };
+                {
+                    response_sequence seq = error<my_exception>(2,"hoge");
+                    AssertThrows(my_exception, seq.get_response({}));
+                    AssertThat(LastException<my_exception>().a, Equals(2));
+                    AssertThat(LastException<my_exception>().b, Equals("hoge"));
+                }
+                {
+                    response_sequence seq = error<my_exception>("fuga");
+                    AssertThrows(my_exception, seq.get_response({}));
+                    AssertThat(LastException<my_exception>().a, Equals(1));
+                    AssertThat(LastException<my_exception>().b, Equals("fuga"));
+                }
+            });
+        });
+        
+        describe("error(T value)", [&]{
+            it("should throw with the value", [&]{
+                {
+                    response_sequence seq = error(1);
+                    AssertThrows(int, seq.get_response({}));
+                    AssertThat(LastException<int>(), Equals(1));
+                }
+                {
+                    response_sequence seq = error("baz");
+                    using str_t = char const *;
+                    AssertThrows(str_t, seq.get_response({}));
+                    AssertThat(LastException<str_t>(), Equals("baz"));
+                }
+            });
+        });
+    });
+});
+}}
---
+++ webmock/directive/response.hpp
@@ -0,0 +1,75 @@
+#ifndef WEBMOCK_DIRECTIVE_HPP
+#define WEBMOCK_DIRECTIVE_HPP
+
+#include <webmock/response_sequence.hpp>
+
+namespace webmock { namespace directive {
+    template <typename Derived>
+    class response_base {
+    public:
+        using generator_type = response_sequence::response_generator_type;
+        
+    protected:
+        std::size_t count = 1;
+        generator_type generator;
+        
+    public:
+        response_base(generator_type generator) : generator(generator) {}
+        
+        Derived & times(std::size_t count) {
+            this->count = count;
+            return static_cast<Derived &>(*this);
+        }
+        
+        Derived & operator *(std::size_t rop) {
+            return this->times(rop);
+        }
+        
+        operator response_sequence() const {
+            return {this->count, this->generator};
+        }
+    };
+    
+    
+    struct static_response: public response_base<static_response> {
+        static_response(webmock::response const & response) :
+            response_base<static_response>([=](auto &&){
+                return response;
+            }
+        ) {}
+    };
+    
+    inline static_response response(webmock::response const & response) {
+        return {response};
+    }
+    
+    
+    struct dynamic_response: public response_base<dynamic_response> {
+        using response_base<dynamic_response>::response_base;
+    };
+
+    inline dynamic_response response(dynamic_response::generator_type generator) {
+        return {generator};
+    }
+    
+    
+    struct error_response: public response_base<error_response> {
+        using response_base<error_response>::response_base;
+    };
+    
+    template <typename Exception, typename... Types>
+    error_response error(Types... args) {
+        return {[=](auto &&) -> webmock::response {
+            throw Exception(args...);
+        }};
+    }
+    
+    template <typename T>
+    error_response error(T value) {
+        return {[=](auto &&) -> webmock::response {
+            throw value;
+        }};
+    }
+}}
+
+#endif
