4014cab21f1c01a19dc7efaec378a20ae2139f6d
Yuichi Murata <mrk21info+github@gmail.com>
2014-12-18 21:30:48 +0900

    Added the directive/mock.

--- test/spec/directive/mock_spec.cpp
+++ test/spec/directive/mock_spec.cpp
@@ -6,9 +6,35 @@ go_bandit([]{
     using namespace bandit;
     
     describe("webmock::directive::mock", []{
-        describe("mock(method, url)", [&]{
-            it("should build", [&]{
+        describe("#count()", [&]{
+            it("should be a number of an access matched by this conditions, and the number should be cached by first result", [&]{
+                stub_registry registry;
+                registry.access({"GET", "http://www.hogefuga.com/1"});
+                registry.access({"PUT", "http://www.hogefuga.com/2"});
+                registry.access({"GET", "http://www.hogefuga.jp/3"});
+                registry.access({"GET", "http://www.hogefuga.com/4"});
                 
+                mock m{"GET", std::regex("http://www.hogefuga.com/.*"), registry};
+                
+                AssertThat(m.count(), Equals(2));
+                registry.access({"GET", "http://www.hogefuga.com/5"});
+                AssertThat(static_cast<int>(m), Equals(2));
+            });
+        });
+        
+        describe("#operator <<(condition_type rop)", [&]{
+            it("should add the condition_type", [&]{
+                stub_registry registry;
+                registry.access({"POST", "http://www.hogebar.com/"});
+                registry.access({"POST", "http://www.hogebar.com/", {
+                    {"Content-Type", "application/json"}
+                }, "response3"});
+                
+                AssertThat(static_cast<int>(
+                    mock{"POST", "http://www.hogebar.com/", registry}
+                        << with_header("Content-Type", "application/json")
+                        << with_body(std::regex("^response.*$"))
+                ), Equals(1));
             });
         });
     });
--- webmock/directive/mock.hpp
+++ webmock/directive/mock.hpp
@@ -1,13 +1,58 @@
-#ifndef WEBMOCK_DIRECTIVE_STUB_HPP
-#define WEBMOCK_DIRECTIVE_STUB_HPP
+#ifndef WEBMOCK_DIRECTIVE_MOCK_HPP
+#define WEBMOCK_DIRECTIVE_MOCK_HPP
 
-#include <webmock/stub.hpp>
 #include <webmock/directive/registry.hpp>
 #include <webmock/directive/condition.hpp>
+#include <boost/optional.hpp>
 
 namespace webmock { namespace directive {
-    struct request_verifier {
+    class mock {
+    protected:
+        using condition_type = condition_list::condition_type;
         
+        condition_list conditions;
+        stub_registry & registry;
+        boost::optional<std::size_t> mutable cached_count;
+        
+    public:
+        mock(
+            with_url const & url,
+            stub_registry & registry = directive::registry()
+        ) : conditions({url}), registry(registry) {
+            this->after_initialize();
+        }
+        
+        mock(
+            with_method const & method,
+            with_url const & url,
+            stub_registry & registry = directive::registry()
+        ) : conditions({method, url}), registry(registry) {
+            this->after_initialize();
+        }
+        
+        std::size_t count() const {
+            if (!this->cached_count) {
+                this->cached_count = this->registry.count_requests(this->conditions);
+            }
+            return *this->cached_count;
+        }
+        
+        operator std::size_t() const {
+            return this->count();
+        }
+        
+        mock & operator <<(condition_type rop) & {
+            this->conditions.add(rop);
+            return *this;
+        }
+        
+        mock operator <<(condition_type rop) && {
+            this->conditions.add(rop);
+            return *this;
+        }
+        
+    protected:
+        virtual void after_initialize() {}
     };
 }}
 
